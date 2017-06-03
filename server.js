729c0427d00dc67a9063818f4e6c53d74736b004
Nick <nramser21@gmail.com>
2017-06-03 10:29:42 -0400

    Server works locally

--- server.js
+++ server.js
@@ -2,13 +2,41 @@ var express = require('express');
 var URL = require('url');
 var app = express();
 
+var urlDictionary = {};
+var port = process.env.PORT || 3000;
+
+function isUrlValid(url) {
+  var regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+  return regex.test(url);
+}
+
+function generateShortUrl(url) {
+  var code = '' + Math.round(1000 + Math.random() * 9000);
+  urlDictionary[code] = url;
+  return code;
+}
+
 app.get('/', (req, res) => {
   res.send('Hello');
 });
 
-app.get('/:query', (req, res) => {
-  res.send(req.path);
-  console.log(req.path);
+app.get('/new/*', (req, res) => {
+  var url = req.path.slice(5);
+  var translation = {};
+  if (isUrlValid(url)) {
+    var code = generateShortUrl(url);
+    translation.original_url = url;
+    translation.short_url = 'http://' + req.hostname + ':' + port + '/' + code;
+  } else {
+    translation.error = "Not a valid url"
+  }
+
+  res.json(translation);
 });
 
-app.listen(process.env.PORT || 3000);
+app.get('/:query', (req, res) => {
+  var query = req.params.query;
+  res.redirect(urlDictionary[query]);
+})
+
+app.listen(port);
